904f2ce6d7b9d657c6adb401d7906ae70838292e
homarinn <homarinn1230@gmail.com>
2020-04-09 00:51:37 +0900

    一旦commit

--- app/decorators/comic_decorator.rb
+++ app/decorators/comic_decorator.rb
@@ -2,13 +2,10 @@ class ComicDecorator < SingleWorkDecorator
   delegate_all
 
   def images_for_display(width: 360)
+    return unless object.images.attached?
     height = width*5/4
-    if object.images.attached?
-      object.images.each do |image|
-        h.concat h.image_tag image.variant(resize_to_fit: [width, height]).processed, class: 'm-image'
-      end
-    else
-      h.image_tag 'no-image.png', width: width, height: height, class: 'm-image'
+    object.images.each do |image|
+      h.concat h.image_tag image.variant(resize_to_fit: [width, height]).processed, class: 'm-image'
     end
   end
 end
