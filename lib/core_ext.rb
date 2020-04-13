fc6489f8b74a408dd33e8342aea7ea97ef0c02ce
homarinn <homarinn1230@gmail.com>
2020-04-14 03:14:14 +0900

    使わないので一旦削除

--- lib/core_ext.rb
+++
@@ -1,4 +0,0 @@
-Dir[Rails.root.join('lib', 'core_ext', '*.{rb}').to_s].each {|file| require file }
-
-module CoreExt
-end
--- lib/core_ext/string.rb
+++
@@ -1,5 +0,0 @@
-class String
-  def to_bool
-    ActiveRecord::Type::Boolean.new.cast(self)
-  end
-end
