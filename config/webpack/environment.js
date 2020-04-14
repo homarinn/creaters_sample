94329ae11fc407741efab655c3810396c111c946
homarinn <homarinn1230@gmail.com>
2020-04-15 04:33:15 +0900

    push precompile

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -5,8 +5,7 @@ environment.plugins.prepend(
   "Provide",
   new webpack.ProvidePlugin({
     $: "jquery/src/jquery",
-    jQuery: "jquery/src/jquery",
-    Dropzone: 'dropzone'
+    jQuery: "jquery/src/jquery"
   })
 );
 module.exports = environment
